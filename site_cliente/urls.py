b9f890e8690834ec3b48b1c3dbcccc1ce75f5e6b
anderlineks <medeirosanderson31@gmail.com>
2026-04-05 23:37:12 -0300

    Configuração do projeto django

---
+++ site_cliente/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+from home import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.home), #retirar esta linha inteira caso vá testar o site para aparecer que a instalação do django 
+                               #foi feita com sucesso (o que foi feito é pra implementar depois)
+]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
