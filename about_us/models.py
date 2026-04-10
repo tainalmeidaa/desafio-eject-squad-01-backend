99d72d74f3221cf5e3c24142676e9efcbb0ab69d
anderlineks <medeirosanderson31@gmail.com>
2026-04-10 04:59:58 -0300

    apenas coloquei o modelo da tabela de reservas pra esse código

--- about_us/models.py
+++ about_us/models.py
@@ -13,3 +13,11 @@ class Post(models.Model):
         verbose_name = "Postagem"
         verbose_name_plural = "Postagens"
         ordering = ["-published_at"]
+
+class Reserva(models.Model):
+    nome_completo = models.CharField(max_length=200)
+    data_horario = models.DateTimeField()
+    quantidade_pessoas = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nome_completo} - {self.data_horario}"
