cace7e435a2816f627fb70bc966bebf7084f5d93
Sérgio Dantas <sergiodnts828@gmail.com>
2026-04-10 11:55:02 -0300

    configuração da pasta de arquivos estáticos

--- core/settings.py
+++ core/settings.py
@@ -82,7 +82,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "global" / "static"]
 
 MEDIA_URL = "media/"
